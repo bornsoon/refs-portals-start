41ad4f76fa83d5de6b75defb0db418f270d44be3
HL <hyolim922@hotmail.com>
2025-08-10 16:13:42 +0000

    ref

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,36 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  // const [submitted, setSubmitted] = useState(false);
+
+  // function handleChange(event) {
+  //   setSubmitted(false); // key stroke 마다 업데이트 막기
+  //   setEnteredPlayerName(event.target.value);
+  // }
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value); // current를 통해 input 요소의 모든 속성에 접근 가능
+    // playerName.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome {enteredPlayerName || "unknown entity"}
+        {/* Welcome {enteredPlayerName ?? "unknown entity"} */}
+        {/* Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"} */}
+      </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={playerName}
+          type="text"
+          // onChange={handleChange}
+          // value={enteredPlayerName}
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
